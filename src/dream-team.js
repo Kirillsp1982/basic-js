c616b16746b5c676e73783df04227b09e89cb7da
Kirillsp1982 <kirillsp1982@gmail.com>
2020-02-26 09:21:25 +0300

    feat: add RegExp and handle lowercase

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,5 @@
 module.exports = function createDreamTeam(members) {
-	const result = members.map(x => x[0]).sort().join('');
+	const result = members.map(x => x.toUpperCase()).map(x => x.match(/[A-Z]/)).map(x => x[0]).sort().join('');
+	return result;
 	return result;
 };
