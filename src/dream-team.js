899d3227a48a5c4885d03ee791aaab2a404eae60
Kirillsp1982 <kirillsp1982@gmail.com>
2020-02-26 10:37:14 +0300

    feat: add finish solution

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,5 +1,5 @@
 module.exports = function createDreamTeam(members) {
 	if (!Array.isArray(members)) return false;
-	const result = members.map(x => x.toUpperCase()).map(x => x.match(/[A-Z]/)).map(x => x[0]).sort().join('');
+	const result = members.filter(x => typeof(x) == 'string').map(x => x.toUpperCase()).map(x => x.match(/[A-Z]/)).map(x => x[0]).sort().join('');
 	return result;
 };
