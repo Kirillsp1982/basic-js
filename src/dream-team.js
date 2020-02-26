58e5aae1392f9da8e0ca6a0cd92fa7af6f18b8ca
Kirillsp1982 <kirillsp1982@gmail.com>
2020-02-26 10:00:48 +0300

    feat: add return false on wrong type

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,5 +1,5 @@
 module.exports = function createDreamTeam(members) {
+	if (!Array.isArray(members)) return false;
 	const result = members.map(x => x.toUpperCase()).map(x => x.match(/[A-Z]/)).map(x => x[0]).sort().join('');
-	return result;
 	return result;
 };
